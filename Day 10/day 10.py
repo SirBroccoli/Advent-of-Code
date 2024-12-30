3aa61687a078c1e42b8d32125f331ece6598e965
SirBroccoli <GalaxySAB@aol.com>
2024-12-30 14:42:45 -0500

    Solved Day 10

--- Day 10/day 10.py
+++ Day 10/day 10.py
@@ -173,7 +173,6 @@ def walkingTheTrail(puzzle):
         # print(path_x)
         # iteratign over the y axis
         for path_y in range(len(puzzle)):
-            position = puzzle[path_x][path_y]
             base_head_coordinates = (path_x, path_y)
             if puzzle[path_x][path_y] == '0':
                 checkingTheTrail(path_x, path_y, puzzle, peak_locations, base_head_coordinates)
@@ -183,10 +182,155 @@ def walkingTheTrail(puzzle):
 
 
 
-puzzle = puzzleReader()
+# puzzle = puzzleReader()
 
-trailheads = walkingTheTrail(puzzle)
+# trailheads = walkingTheTrail(puzzle)
 
-trail_score = sum(len(i) for i in trailheads.values())
+# trail_score = sum(len(i) for i in trailheads.values())
 
-print(trail_score)
+# print(trail_score) ## 514
+
+'''
+--- Part Two ---
+The reindeer spends a few minutes reviewing your hiking trail 
+map before realizing something, disappearing for a few minutes, 
+and finally returning with yet another slightly-charred piece of 
+paper.
+
+The paper describes a second way to measure a trailhead called 
+its rating. A trailhead's rating is the number of distinct hiking 
+trails which begin at that trailhead. For example:
+
+.....0.
+..4321.
+..5..2.
+..6543.
+..7..4.
+..8765.
+..9....
+The above map has a single trailhead; its rating is 3 because 
+there are exactly three distinct hiking trails which begin at 
+that position:
+
+.....0.   .....0.   .....0.
+..4321.   .....1.   .....1.
+..5....   .....2.   .....2.
+..6....   ..6543.   .....3.
+..7....   ..7....   .....4.
+..8....   ..8....   ..8765.
+..9....   ..9....   ..9....
+Here is a map containing a single trailhead with rating 13:
+
+..90..9
+...1.98
+...2..7
+6543456
+765.987
+876....
+987....
+This map contains a single trailhead with rating 227 
+(because there are 121 distinct hiking trails that lead to
+the 9 on the right edge and 106 that lead to the 9 on the 
+bottom edge):
+
+012345
+123456
+234567
+345678
+4.6789
+56789.
+Here's the larger example from before:
+
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+Considering its trailheads in reading order, they have 
+ratings of 20, 24, 10, 4, 1, 4, 5, 8, and 5. The sum of 
+all trailhead ratings in this larger example topographic 
+map is 81.
+
+You're not sure how, but the reindeer seems to have crafted 
+some tiny flags out of toothpicks and bits of paper and is 
+using them to mark trailheads on your topographic map. What 
+is the sum of the ratings of all trailheads?
+'''
+
+def checkingTheTrailPartTwo(x, y, puzzle, peak_locations, start_of_trail):
+    '''
+    It doesn't matter now that we only track one peak per trail head
+    keep track of all of the different ways to reach a peak from a starting point
+    returns a dictionary of all of the ways to reach a peak from a single point
+    '''
+    
+    current_position = int(puzzle[x][y])
+    curr_coordinates = (int(x), int(y))
+    print(f'position {x, y} {current_position}')
+
+    if current_position == 9: 
+        print('Current position is a 9')
+        
+        # from the trail head (0), has this peak been visited before?
+        if start_of_trail not in peak_locations:
+            print('initializing peak_locations for start_of_trail')
+            peak_locations[start_of_trail] = 1
+       
+        # a trail head (0) can visit a peak (curr_coordinates) many times, but only be considered one trail
+        # have we visited this peak from the trail head before?
+        # if not, add the peak value to the trail head Key
+        # if curr_coordinates not in peak_locations[start_of_trail]:
+        else:
+            print('adding position to peak_locations')
+            peak_locations[start_of_trail] += 1
+
+    # is north one value greater than our current position?
+    if x > 0 and int(puzzle[x-1][y]) == int(current_position) + 1:
+        checkingTheTrailPartTwo(x - 1, y, puzzle, peak_locations, start_of_trail)
+    
+    # is East one value greater than our current position?
+    if y < len(puzzle)-1 and int(puzzle[x][y+1]) == int(current_position) + 1:
+        checkingTheTrailPartTwo(x, y+1, puzzle, peak_locations, start_of_trail)
+
+    # is South one value greater than our current position?
+    if x < len(puzzle)-1 and int(puzzle[x+1][y]) == int(current_position) + 1:
+        checkingTheTrailPartTwo(x + 1, y, puzzle, peak_locations, start_of_trail)
+    
+    # is West one value greater than our current position?  
+    if y > 0 and int(puzzle[x][y-1]) == int(current_position) + 1:
+        checkingTheTrailPartTwo(x, y-1, puzzle, peak_locations, start_of_trail)
+
+def walkingTheTrailPartTwo(puzzle):
+    '''
+    iterates over the puzzle file,
+    Identifying any zero values as the start of the trailhead
+    It will call checkinTheTrail function through recursion to identify connecting values that will
+    lead to a 9 value through every iteration
+    '''
+    peak_locations = {} # identify the top of the trailheads in x, y format
+
+    # iterate over the x axis
+    for path_x in range(len(puzzle)):
+        # print(path_x)
+        # iteratign over the y axis
+        for path_y in range(len(puzzle)):
+            base_head_coordinates = (path_x, path_y)
+            if puzzle[path_x][path_y] == '0':
+                checkingTheTrailPartTwo(path_x, path_y, puzzle, peak_locations, base_head_coordinates)
+
+    print(peak_locations)
+    return peak_locations
+
+part_two_puzzle = puzzleReader()
+
+trailheads_part_two = walkingTheTrailPartTwo(part_two_puzzle)
+print(trailheads_part_two)
+print(trailheads_part_two.values())
+
+total = 0
+for i in trailheads_part_two.values():
+    total += 1
+print(total)
